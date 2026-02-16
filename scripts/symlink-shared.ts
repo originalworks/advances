f7d44d97d791669c3d8260e8e283285127cd91a6
Daniel Cybulka <danielcybulka@gmail.com>
2026-02-16 11:11:47 +0100

    remove leftovers

--- scripts/symlink-shared.ts
+++ scripts/symlink-shared.ts
@@ -10,7 +10,7 @@ const sharedContractsPath = path.join(
 );
 
 // List your contracts libs here
-const targets = ['contracts-agreements', 'contracts-advances'];
+const targets = ['contracts-advances'];
 
 for (const target of targets) {
   const targetSharedPath = path.join(
