97c559f32df2e3d656b3b5932d979e95fcfa76f7
Daniel Cybulka <danielcybulka@gmail.com>
2026-02-23 13:59:17 +0100

    fix formatting

--- libs/contracts-shared/contracts/ERC20TokenMock/ERC20TokenMock.sol
+++ libs/contracts-shared/contracts/ERC20TokenMock/ERC20TokenMock.sol
@@ -6,64 +6,64 @@ import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
 import './EIP3009.sol';
 
 contract ERC20TokenMock is
-  ERC20Upgradeable,
-  OwnableUpgradeable,
-  EIP3009,
-  UUPSUpgradeable
+    ERC20Upgradeable,
+    OwnableUpgradeable,
+    EIP3009,
+    UUPSUpgradeable
 {
-  string public constant version = '2';
-  uint8 private _decimalPoints;
+    string public constant version = '2';
+    uint8 private _decimalPoints;
 
-  /// @custom:oz-upgrades-unsafe-allow constructor
-  constructor() {
-    _disableInitializers();
-  }
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
 
-  function initialize(
-    string memory name,
-    string memory symbol,
-    uint8 decimalPoints
-  ) public initializer {
-    __ERC20_init(name, symbol);
-    __EIP712_init_unchained(name, '2');
-    __Ownable_init(msg.sender);
-    __UUPSUpgradeable_init();
-    _decimalPoints = decimalPoints;
-  }
+    function initialize(
+        string memory name,
+        string memory symbol,
+        uint8 decimalPoints
+    ) public initializer {
+        __ERC20_init(name, symbol);
+        __EIP712_init_unchained(name, '2');
+        __Ownable_init(msg.sender);
+        __UUPSUpgradeable_init();
+        _decimalPoints = decimalPoints;
+    }
 
-  function mintTo(address user, uint256 amount) public {
-    _mint(user, amount);
-  }
+    function mintTo(address user, uint256 amount) public {
+        _mint(user, amount);
+    }
 
-  function decimals() public view override returns (uint8) {
-    return _decimalPoints;
-  }
+    function decimals() public view override returns (uint8) {
+        return _decimalPoints;
+    }
 
-  function transferWithAuthorization(
-    address from,
-    address to,
-    uint256 value,
-    uint256 validAfter,
-    uint256 validBefore,
-    bytes32 nonce,
-    uint8 v,
-    bytes32 r,
-    bytes32 s
-  ) public {
-    _transferWithAuthorization(
-      from,
-      to,
-      value,
-      validAfter,
-      validBefore,
-      nonce,
-      v,
-      r,
-      s
-    );
-  }
+    function transferWithAuthorization(
+        address from,
+        address to,
+        uint256 value,
+        uint256 validAfter,
+        uint256 validBefore,
+        bytes32 nonce,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public {
+        _transferWithAuthorization(
+            from,
+            to,
+            value,
+            validAfter,
+            validBefore,
+            nonce,
+            v,
+            r,
+            s
+        );
+    }
 
-  function _authorizeUpgrade(
-    address newImplementation
-  ) internal override onlyOwner {}
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlyOwner {}
 }
